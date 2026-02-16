198ab4bce3f2962d6fc47d1e30d15fe486826b5f
Aymanesaj <sajidaymane@gmail.com>
2026-02-16 00:18:20 +0000

    Serve HTML page

--- app/test.py
+++ app/test.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello"
+    return send_file("app.html")
 
 @app.route("/health")
 def health():
     return "Ok"
 
 app.run(host="0.0.0.0", port=8080)
+
